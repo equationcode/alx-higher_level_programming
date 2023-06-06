32c837072fcdefd94dcd2547d3c1fd4aa3fcbb4d
equationcode <annypaul80@gmail.com>
2023-06-05 19:36:23 -0700

    10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * check_cycle - function that checks for a linked list contains a cycle
+ * @list: the linked list to be check
+ *
+ * Return: 1 if the list has a cycle, 0 if it doesn't
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list;
+
+	if (!list)
+		return (0);
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+
+	return (0);
+}

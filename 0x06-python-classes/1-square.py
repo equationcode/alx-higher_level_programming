112cce9852e9071f7b84f7fa8342a3ebbdc9eb63
equationcode <annypaul80@gmail.com>
2023-06-27 06:55:26 -0700

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 
-"Define a class Square"
+"Define a class Square."
 
 class Square:
-    "Represent a Square"
+    "Represent a Square."
 
     def_init_(self, size):
         "Initialize a new Square.
 
         Args:
-        size (int): The size of the new square"
+        size (int): The size of the new square.
+        "
 
-        self_size = size
+        self._size = size

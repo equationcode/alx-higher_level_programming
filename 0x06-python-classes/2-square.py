208c35acdfa022ed3f022181ed5e09bdfa5c03a5
equationcode <annypaul80@gmail.com>
2023-06-27 07:38:33 -0700

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
+
 "Define a class Square."
 
 
 class Square:
-    "Represent a square."
+   "Represent a square."
 
+    def __init__(self, size=0):
+        "Initialize a new Square.
 
-    def _init_(self, size=0);
-    "Initialize a new Square.
-
-
-    Args:
-        size (int): The size of the new square.
+        Args:
+            size (int): The size of the new square.
         "
-    if not is instance(size, int):
-        raise TypeError("size must be an integer")
-    elif size < 0:
-        raise ValueError("size must be >= 0")
-    self._size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size

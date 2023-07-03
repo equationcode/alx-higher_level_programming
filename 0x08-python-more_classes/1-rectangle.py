032a0e1281e00213e2ffcde4cff697ce4086ce1f
equationcode <annypaul80@gmail.com>
2023-07-03 12:43:04 -0700

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,38 +5,33 @@
 class Rectangle:
     """Represent a rectangle."""
 
-    def _init_(self, width=0, height=0):
+    def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
 
-
         Args:
-        width (int): The width of the new rectangle.
-        height (int): The height of the new rectangle.
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
         """
         self.width = width
         self.height = height
 
-
     @property
     def width(self):
-        """set the width of the rectangle."""
-        return self._width
-
+        """Set the width of the rectangle."""
+        return self.__width
 
     @width.setter
     def width(self, value):
-        if not isintance(value, int):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("width must be >= )")
-        self._width = value
-
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
         """Set the height of the rectangle."""
-        return self._height
-
+        return self.__height
 
     @height.setter
     def height(self, value):
@@ -44,4 +39,4 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self._height = value
+        self.__height = value

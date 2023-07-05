f0b5ffe61174937bbf5bc9697c485f508e08a618
equationcode <annypaul80@gmail.com>
2023-07-05 08:20:19 -0700

    101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -4,7 +4,9 @@
 Determines all possible solutions to placing N
 N non-attacking queens on an NxN chessboard.
 
-$ ./101-nqueens.py N
+
+    $ ./101-nqueens.py N
+
 
 N must be an integer greater than or equal to 4.
 

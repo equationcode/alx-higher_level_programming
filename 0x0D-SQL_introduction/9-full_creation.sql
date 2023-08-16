4ecfba710efb81193b0abbf7d9aa706a53ac8cb2
equationcode <annypaul80@gmail.com>
2023-08-15 20:02:12 -0700

    9-full_creation.sql

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+-- script that creates a table second_table in the database hbtn_0c_0 in MySQL server and add multiples rows.
+-- If the table second_table already exists, script should not fail
+
+CREATE table IF NOT EXISTS second_table (`id` INT,
+`name` VARCHAR(256),
+`score` INT);
+INSERT INTO `second_table` (`id`, `name`, `score`)
+VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);

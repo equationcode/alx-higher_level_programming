bed24c99e9fb3a48ac0684162cdf23a378845426
equationcode <annypaul80@gmail.com>
2023-09-28 17:15:07 -0700

    3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -s1 -X OPTIONS "$1" | grep -oiE 'ALLOW": .+' | cut -d ' ' -f2
+curl -s1 -X OPTIONS "$1" | grep -oiE 'ALLOW": .+' | cut -d ' ' -f2-
